cb40a86f08dd6effa7a145166a85002c5f6d12ef
Your Name <you@example.com>
2022-09-03 10:59:48 +0300

    add zadaca2

---
+++ zadaca2/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, 
+// что третьей цифры нет.
+
+Console.WriteLine("Введите число");
+int number = int.Parse(Console.ReadLine());
+if (number >= 100)
+    {
+        Console.WriteLine($"Третья цифра чиcла равна {number % 10}");
+    }
+else
+    {
+        Console.WriteLine($"Число состоить менье чем из трех цифр");
+    }
